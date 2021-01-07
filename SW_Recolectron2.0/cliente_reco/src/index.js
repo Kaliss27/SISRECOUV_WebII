577d58b6ab4ac214b8e6ec5b98ac370a0732ab9f
Karen Peralta <karenlisspelu10@outlook.com>
2021-01-07 03:07:14 -0600

    Update
    -Vista Inicio
    -Imagenes

--- SW_Recolectron2.0/cliente_reco/src/index.js
+++ SW_Recolectron2.0/cliente_reco/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import Blog from './components/Blog'
+import Blog from './pages/InicioReco'
 
 const container = document.getElementById('root');
 
